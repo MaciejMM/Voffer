5e2f61675b6d65e16626cbbb1ce8533afeec454e
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2025-02-10 00:10:19 +0100

    update kinde urls

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,7 +3,7 @@ export const environment ={
   backendUrl: 'https://vehicle-offer-uat-be5f6f1d949e.herokuapp.com',
   kindeDomain: 'https://voffer-lkkhkbrq52jbt3t.eu.kinde.com',
   kindeClientId: '24694b8836994e2fa5bf9a5e5f19e9e4',
-  kindeRredirectURL: 'https://vehicle-offer-uat-be5f6f1d949e.herokuapp.com/',
-  kindeLogoutRedirectURL: 'https://vehicle-offer-uat-be5f6f1d949e.herokuapp.com/',
+  kindeRredirectURL: 'https://voffer-d18ce4ed1b53.herokuapp.com/',
+  kindeLogoutRedirectURL: 'https://voffer-d18ce4ed1b53.herokuapp.com/',
   name: 'test',
 }
